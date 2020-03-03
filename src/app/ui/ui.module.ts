d574cbb80944821b5248e718a246a4b89e829057
ChristopheGueroult <christophegueroult@gmail.com>
2020-03-03 17:50:41 +0100

    router

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -4,14 +4,17 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { UiComponent } from './components/ui/ui.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [UiComponent, HeaderComponent, NavComponent, FooterComponent],
   exports: [UiComponent, HeaderComponent, NavComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
+    RouterModule
   ]
 })
 export class UiModule { }
